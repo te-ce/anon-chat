08f917d6a25f336c7e69e40f5ad5d0f27027b88d
te-ce <9517685+te-ce@users.noreply.github.com>
2025-12-09 11:36:59 +0100

    next config: ignore ts and eslint build errors

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,14 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
+const nextConfig: NextConfig & { eslint: { ignoreBuildErrors: boolean } } = {
   /* config options here */
   reactCompiler: true,
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  eslint: {
+    ignoreBuildErrors: true,
+  },
 };
 
 export default nextConfig;
