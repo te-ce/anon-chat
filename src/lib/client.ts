31e9ad577a697ad56a4de0f69dcb98ccab8803aa
te-ce <9517685+te-ce@users.noreply.github.com>
2025-12-09 15:53:03 +0100

    api client: add env for api route

--- src/lib/client.ts
+++ src/lib/client.ts
@@ -1,4 +1,6 @@
 import { treaty } from "@elysiajs/eden";
 import type { App } from "../app/api/[[...slugs]]/route";
 
-export const client = treaty<App>("localhost:3000").api;
+const apiRoute = process.env.NEXT_PUBLIC_API_ROUTE ?? "http://localhost:3000";
+
+export const client = treaty<App>(apiRoute).api;
